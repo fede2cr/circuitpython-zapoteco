6b68d59ea7eaca9b5b00652e3cdcc08cf4e57c75
Alvaro Figueroa <alvaro@greencore.co.cr>
2019-02-11 21:38:22 -0600

    Agregando code.py con diccionario 1-20

---
+++ code.py
@@ -0,0 +1,9 @@
+
+num_zapoteco = [ "0", "Chāga", "Cāto", "Cāyo", "Taà", "Gaàyú",
+        "Guàyú-bichāga", "Guàyú-bicāto", "Guàyú-bicāyo", "Guàyú-bitaà", "Chiì",
+        "Chiì-bichāga", "Chiì-bicāto", "Chiì-bicāyo", "Chiì-bitaà", "Chiìnu",
+        "Chiìnu-bichāga", "Chiìnu-bicāto", "Chiìnu-bicāyo", "Chiìnu-bitaà", "Gāndé"]
+
+print(num_zapoteco[1])
+print(num_zapoteco[11])
+print(num_zapoteco[20])
