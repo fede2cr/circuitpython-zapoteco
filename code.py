ed9f6db9bcd8683c6f37503ec36be6e7c00698d9
Alvaro Figueroa <alvaro@greencore.co.cr>
2019-02-13 23:04:19 -0600

    Agregando números en Bribri

--- code.py
+++ code.py
@@ -4,6 +4,9 @@ num_zapoteco = [ "0", "Chāga", "Cāto", "Cāyo", "Taà", "Gaàyú",
         "Chiì-bichāga", "Chiì-bicāto", "Chiì-bicāyo", "Chiì-bitaà", "Chiìnu",
         "Chiìnu-bichāga", "Chiìnu-bicāto", "Chiìnu-bicāyo", "Chiìnu-bitaà", "Gāndé"]
 
+num_bribri = [ "0", 'E\'köl', 'Böl', 'Māñál,'Tkél', 'Skél',
+        'Tèröl, 'Kúl', 'Pàköl', 'Sūlítu', 'Dabòm')
+
 print(num_zapoteco[1])
 print(num_zapoteco[11])
 print(num_zapoteco[20])
